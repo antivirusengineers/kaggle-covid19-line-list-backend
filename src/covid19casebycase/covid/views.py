d5a5ef17c5bd5c358cf7ed323d8210f21bc75df1
Ravi Patel <patelr3@cs.washington.edu>
2020-03-29 13:50:27 -0700

    Adds other routes w/ dummy data

--- src/covid19casebycase/covid/views.py
+++ src/covid19casebycase/covid/views.py
@@ -27,9 +27,93 @@ def _getSymptomPercentageCountry(country_name, attribute_name, attribute_value):
 
 def getSymptomPercentageState(request):
     body = json.loads(request.body)
-    return HttpResponse("Here is state %s percentage.")
+    resp_dict = {}
+
+    location = body["location"]
+    attributes = body["attributes"]
+
+    resp_dict["state"] = location
+    resp_dict["attributePercentages"] = {}
+
+    for field in attributes:
+        percentage = _getSymptomPercentageCountry(location, field, attributes[field])
+        resp_dict["attributePercentages"][field] = percentage
+
+    resp_body = json.dumps(resp_dict)
+    return HttpResponse(resp_body)
 
 def getSymptomPercentageCounty(request):
     body = json.loads(request.body)
-    return HttpResponse("Here is county %s percentage.")
+    resp_dict = {}
+
+    location = body["location"]
+    attributes = body["attributes"]
+
+    resp_dict["county"] = location
+    resp_dict["attributePercentages"] = {}
+
+    for field in attributes:
+        percentage = _getSymptomPercentageCountry(location, field, attributes[field])
+        resp_dict["attributePercentages"][field] = percentage
+
+    resp_body = json.dumps(resp_dict)
+    return HttpResponse(resp_body)
     
+def getSymptoms(request):
+    resp_dict = {}
+
+    resp_dict["symptoms"] = _getSymptoms()
+    
+    resp_body = json.dumps(resp_dict)
+    return HttpResponse(resp_body)
+
+def _getSymptoms():
+    symptoms =  []
+    symptoms.append("da flu")
+    symptoms.append("da corona")
+    return symptoms
+
+def getCountries(request):
+    resp_dict = {}
+
+    resp_dict["countries"] = _getCountries()
+
+    resp_body = json.dumps(resp_dict)
+    return HttpResponse(resp_body)
+
+def _getCountries():
+    countries = []
+    countries.append("da usa")
+    countries.append("ooo canada")
+    return countries
+
+def getStates(request):
+    resp_dict = {}
+
+    resp_dict["states"] = _getStates()
+
+    resp_body = json.dumps(resp_dict)
+    return HttpResponse(resp_body)
+
+def _getStates():
+    states = []
+    states.append("wurshingtun")
+    states.append("kanto")
+
+    return states
+
+def getCounties(request):
+    resp_dict = {}
+
+    resp_dict["counties"] = _getCounties()
+
+    resp_body = json.dumps(resp_dict)
+    return HttpResponse(resp_body)
+
+def _getCounties():
+    counties = []
+    counties.append("king")
+    counties.append("queen")
+    counties.append("pallet town")
+
+    return counties
