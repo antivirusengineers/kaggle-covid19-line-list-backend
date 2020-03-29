964a0b665b8100439cfa7224cf328f6a03297143
Ravi Patel <patelr3@cs.washington.edu>
2020-03-29 13:50:27 -0700

    Adds other routes w/ dummy data

--- src/covid19casebycase/covid/urls.py
+++ src/covid19casebycase/covid/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('getSymptomPercentageCountry', views.getSymptomPercentageCountry, name='getSymptomPercentageCountry'),
     path('getSymptomPercentageState', views.getSymptomPercentageState, name='getSymptomPercentageState'),
     path('getSymptomPercentage', views.getSymptomPercentageCounty, name='getSymptomPercentageCounty'),
+    path('getSymptoms', views.getSymptoms, name='getSymptoms'),
+    path('getCountries', views.getCountries, name='getCountries'),
+    path('getStates', views.getStates, name='GetStates'),
+    path('getCounties', views.getCounties, name='GetCounties'),
 ]
