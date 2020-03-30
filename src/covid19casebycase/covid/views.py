9a4131f62adbb1ad423f1edb35050d8911ed07b9
Ravi Patel <patelr3@cs.washington.edu>
2020-03-29 21:09:07 -0700

    Removes unnecessary endpoints

--- src/covid19casebycase/covid/views.py
+++ src/covid19casebycase/covid/views.py
@@ -52,46 +52,6 @@ def getSymptomPercentageCountry(request):
 
     resp_body = json.dumps(resp_dict)
     return HttpResponse(resp_body)
-
-def _getSymptomPercentageState(state_name, attribute_name, attribute_value):
-    return 0.20
-
-def getSymptomPercentageState(request):
-    resp_dict = {}
-
-    location = body["location"]
-    attributes = body["attributes"]
-
-    resp_dict["state"] = location
-    resp_dict["attributePercentages"] = {}
-
-    for field in attributes:
-        percentage = _getSymptomPercentageState(location, field, attributes[field])
-        resp_dict["attributePercentages"][field] = percentage
-
-    resp_body = json.dumps(resp_dict)
-    return HttpResponse(resp_body)
-
-def _getSymptomPercentageCounty(county_name, attribute_name, attribute_value):
-    return 0.30
-
-def getSymptomPercentageCounty(request):
- 
-    resp_dict = {}
-
-    location = request.GET.get("location")
-   
-    attributes = request.GET.get("attributes")
-
-    resp_dict["county"] = location
-    resp_dict["attributePercentages"] = {}
-
-    for field in attributes:
-        percentage = _getSymptomPercentageCounty(location, field, attributes[field])
-        resp_dict["attributePercentages"][field] = percentage
-
-    resp_body = json.dumps(resp_dict)
-    return HttpResponse(resp_body)
     
 def getSymptoms(request):
     resp_dict = {}
@@ -127,38 +87,8 @@ def _getGenders():
 def _getCountries():
     return  list(Case.objects.order_by("country").values_list('country', flat=True).distinct())
 
-def getStates(request):
-    resp_dict = {}
-
-    resp_dict["states"] = _getStates()
-
-    resp_body = json.dumps(resp_dict)
-    return HttpResponse(resp_body)
-
-def _getStates():
-    states = []
-    states.append("wurshingtun")
-    states.append("kanto")
-
-    return states
-
-def getCounties(request):
-    resp_dict = {}
-
-    resp_dict["counties"] = _getCounties()
-
-    resp_body = json.dumps(resp_dict)
-    return HttpResponse(resp_body)
-
-def _getCounties():
-    counties = []
-    counties.append("king")
-    counties.append("queen")
-    counties.append("pallet town")
-
-    return counties
-
 def updateDB(request): 
     from .parser import parseKaggleCSV
 
     parseKaggleCSV()
+    return HttpResponse('Success')
